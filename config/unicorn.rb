98306bf8001d508e6c4d028fafc2c81efb979c35
shosei666 <65118535+shosei666@users.noreply.github.com>
2020-06-20 17:25:11 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,15 +8,16 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-#ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
+#ポート番号を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
