da4f7d3ab9a4a2dff35764dd80f4e2f1d571b37b
shosei666 <65118535+shosei666@users.noreply.github.com>
2020-06-24 18:34:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,23 +25,15 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :linked_files, %w{ config/secrets.yml }
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-end
 
-set :linked_files, %w{ config/secrets.yml }
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
 
   desc 'upload secrets.yml'
   task :upload do
